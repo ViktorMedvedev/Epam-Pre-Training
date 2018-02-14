357bae16257405aa716997f7dbdd93eb16827225
ViktorMedvedev <vitreonik@gmail.com>
2018-02-14 07:37:54 +0300

    fixed4

--- Task4/src/by/epam/preTraining/task4/sumOfDigits/view/Printer.java
+++ Task4/src/by/epam/preTraining/task4/sumOfDigits/view/Printer.java
@@ -1,7 +1,5 @@
 package by.epam.preTraining.task4.sumOfDigits.view;
 
-import by.epam.preTraining.task4.sumOfDigits.model.SumCounter;
-
 public class Printer {
     public static void printSumOfDigits(String message){
         System.out.println(message);
